85e32279629c6a0b83263abf62c171fd0f4f48eb
bhargavlenin <bhargavlenin123@users.noreply.github.com>
2018-06-06 13:56:06 -0500

    Initial commit

---
+++ src/com/lenins/company/Main.java
@@ -0,0 +1,15 @@
+package com.lenins.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x5=0;
+
+        for(int i=0;i<1000; i++){
+            if(i%3==0 || i%5==0){
+                x5 += i;
+            }
+        }
+        System.out.println(x5);
+    }
+}
